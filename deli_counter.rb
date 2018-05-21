76746c2ca0abf60e3f2c88a793e08b26ccacfab1
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-21 16:27:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,11 @@ katz_deli = []
 def line(katz_deli)
   katz_deli_customers = []
   katz_deli.each.with_index(1) do | name, position |
-  katz_deli_customers << #{position}. #{name}
+  katz_deli_customers << "#{position}. #{name}"
   end
   if katz_deli == []
     puts "The line is currently empty."
   else
     puts katz_deli_customers
   end
-end
-
 end

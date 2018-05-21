df91d0df7d7d9a3ecf1e1cc4bfd6996dcb408b33
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-21 16:30:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,6 @@ def line(katz_deli)
   katz_deli_customers = []
   katz_deli.each.with_index(1) do | name, position |
   katz_deli_customers << "#{position}. #{name}"
-  end
   katz_deli_customers
 end
   if katz_deli == []

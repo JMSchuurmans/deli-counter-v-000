6002b4df777a0b9f471581d282a0d092a196078d
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-21 16:28:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,6 +8,6 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    puts katz_deli_customers
+    puts The line is currently: katz_deli_customers
   end
 end
